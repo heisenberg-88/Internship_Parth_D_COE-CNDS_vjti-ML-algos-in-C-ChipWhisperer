58010c0e4ad1c586a3a9bd20030ad6adfa6dcd5f
parth <parthprd@outlook.com>
2022-06-07 17:30:46 +0530

    LinearReg PreFinal

--- LinearReg.c
+++ LinearReg.c
@@ -3,12 +3,14 @@
 #include <math.h>
 
 
+
 // sigmoid of whole array
 // tested & working
 float sigmoid(float n){
     float s = 1/(1 + exp(-n));
     return s;
 }
+
 
 
 
@@ -25,38 +27,39 @@ float* weightinit(int dim){
 
 
 
+static float x_train[12288][50];
+static float y_train[50];
+static int NoOfEx = 50;
+
+
+
+
+
 //forward propagation 
 //tested and working
-// put 2nd dimention in input x array
-float* propagate(float* w, float b ,float x[][3],float y[], int dim , int NoOfEx){
-    
+float* propagate(float* w, float b , int dim ){
+
     // activated value calculated for each value
     // tested and working 
+    
     float A[NoOfEx];
     for(int i=0;i<NoOfEx;i++){
-        // printf("Example no : %d \n",i);
-        float temp = 0;
+        float temp = 0.0;
         for(int j=0;j<dim;j++){
-            // printf("multiplying %f with %f \n",w[j],x[j][i]);
-            temp+=(w[j]*x[j][i]);
+            temp+=(w[j]*x_train[j][i]);
         }
-        // printf("current temp value: %f \n",temp+b);
         A[i] = sigmoid(temp+b);
     }
-    // for(int i=0;i<NoOfEx;i++){
-    //     printf("%f \n",A[i]);
-    // }
-
-
+    
+    
 
     // cost calculated considering all values
     //tested and working
     float cost = 0;
     for(int i=0;i<NoOfEx ;i++){
-        cost += ((y[i]*(-log(A[i])) + (1-y[i])*(-log(1-A[i])))); 
+        cost += ((y_train[i]*(-log(A[i])) + (1-y_train[i])*(-log(1-A[i])))); 
     }
     cost/=NoOfEx;
-    // printf("the cost value : %f \n",cost);
 
 
 
@@ -65,25 +68,19 @@ float* propagate(float* w, float b ,float x[][3],float y[], int dim , int NoOfEx
     float *dw = malloc(sizeof(float) * (dim+2));
     float temp1[NoOfEx];
     for(int i=0;i<NoOfEx;i++){
-        // printf("sub %f - %f \n",A[i],y[i]);
-        temp1[i] = (A[i]-y[i]);
+        temp1[i] = (A[i]-y_train[i]);
     }
-    // printf("values in temp1 are: \n");
-    // for(int i=0;i<NoOfEx;i++){
-    //     printf("%f \n",temp1[i]);
-    // }
+
+    
     for(int i=0;i<dim;i++){
         float temp2 = 0;
         for(int j=0;j<NoOfEx;j++){
-            temp2 += (temp1[j]*x[i][j]);
+            temp2 += (temp1[j]*x_train[i][j]);
         }
         temp2/=NoOfEx;
         dw[i] = temp2;
     }
-    // printf("the dw values are : \n");
-    // for(int i=0;i<dim;i++){
-    //     printf("%f \n",dw[i]);
-    // }
+    
 
     
     float db = 0;
@@ -93,8 +90,6 @@ float* propagate(float* w, float b ,float x[][3],float y[], int dim , int NoOfEx
     db/=NoOfEx;
     dw[dim] = db;
     dw[dim+1] = cost;
-    // printf("db value: %f \n",dw[dim]);
-    // printf("cost value: %f \n",dw[dim+1]);
 
 
     return dw;
@@ -106,13 +101,13 @@ float* propagate(float* w, float b ,float x[][3],float y[], int dim , int NoOfEx
 
 
 
+
 // optimization function
 // tested and working
-// put 2nd dimention in input x array
-float* optimize(float* w,float b,float x[][3],float y[],int num_iterations,float learning_rate, int dim ,int NoOfEx){
+float* optimize(float* w,float b,int num_iterations,float learning_rate, int dim ){
 
     // new weights vector for updating
-    float wnew[dim];
+    float *wnew = malloc(sizeof(float) * (dim));
     for(int i=0;i<dim;i++){
         wnew[i] = w[i];
     }
@@ -128,28 +123,23 @@ float* optimize(float* w,float b,float x[][3],float y[],int num_iterations,float
     // tested and working
     for(int i = 0;i<num_iterations;i++){
 
-        float* dwinfo = propagate(wnew,bnew,x,y,dim,NoOfEx);
+        float* dwinfo = propagate(wnew,bnew,dim);
         for(int i=0;i<dim;i++){
             dw[i] = dwinfo[i];
         }
         db = dwinfo[dim];
         cost = dwinfo[dim+1];
 
-
         //updating weights and bias
         for(int i=0;i<dim;i++){
-            // printf("new wnew is : %f - ((%f)*(%f)) )",wnew[i],dw[i],learning_rate);
-            // printf("\n");
             wnew[i] = wnew[i] - ((dw[i])*(learning_rate));
         }
-        // for(int i=0;i<dim;i++){
-        //     printf("new wnew values are: %f \n",wnew[i]);
-        // }
+
+
         bnew = bnew - ((db)*(learning_rate));
-        // printf("new bnew values are: %f \n",bnew);
 
         if(i%100==0){
-            printf("cost after %d iterations is %f \n",i+1,cost);
+            printf("cost/loss after %d iterations is %f \n",i+1,cost);
         }
     }
 
@@ -179,28 +169,21 @@ float* optimize(float* w,float b,float x[][3],float y[],int num_iterations,float
 
 
 
+
 // predict outputs y_preds
 // tested and working
-// put 2nd dimention in input x array
-float* predict(float* w,float b, float x[][3],int dim,int NoOfEx){
-
-    float *y_predictions = malloc(sizeof(float) * (dim));
+float* predict(float* w,float b,int dim){
 
     // calculating sigmoid values
     float A[NoOfEx];
     for(int i=0;i<NoOfEx;i++){
-        float temp = 0;
+        float temp = 0.0;
         for(int j=0;j<dim;j++){
-            temp+=(w[j]*x[j][i]);
+            temp+=(w[j]*x_train[j][i]);
         }
         A[i] = sigmoid(temp+b);
     }
 
-    // printf("These are A[i] values \n");
-    // for(int i=0;i<NoOfEx;i++){
-    //     printf("%f \n",A[i]);
-    // }
-    // printf("------------ \n");
 
     float* y_preds = malloc(sizeof(float)*dim);
 
@@ -221,35 +204,33 @@ float* predict(float* w,float b, float x[][3],int dim,int NoOfEx){
 
 
 
+
+
 // printing the accuracy
 // testing
-// put 2nd dimention in input x array
-void printaccuracy(float x_train[][3],float y_train[],float x_test[][3],float y_test[]){
-    float* w;
-    int dimention ;  //hardcode the value here
-    
+void printaccuracy(int dimention){
+
 
     // initialize weights and biases
-    w = weightinit(dimention);
+    float* w = weightinit(dimention);
     float b = 0.0;
     
 
     // optimize function for optimizing weights and biases
-    float* infovec = optimize(w,b,x_train,y_train,2000,0.5,dimention);
+    float* infovec = optimize(w,b,2000,0.005,dimention);
 
     float wfinal[dimention];
     float bfinal = 0.0;
+
 
     for(int i=0;i<dimention;i++){
         wfinal[i] = infovec[i];
     }
     bfinal = infovec[dimention+dimention];
 
-    float* y_preds_train;
-    float* y_preds_test;
 
-    y_preds_train = predict(wfinal,bfinal,x_train,dimention,NoOfEx);
-    y_preds_test = predict(wfinal,bfinal,x_test,dimention,NoOfEx);
+    float* y_preds_train;
+    y_preds_train = predict(wfinal,bfinal,dimention);
 
 
     // calculating training accuracy
@@ -262,44 +243,52 @@ void printaccuracy(float x_train[][3],float y_train[],float x_test[][3],float y_
     accu_train = (100 - (temp)*100);
 
 
-    // calculating testing accuracy
-    float accu_test = 0.0;  
-    temp = 0.0;
-    for(int i=0;i<dimention;i++){
-        temp+=(abs(y_test[i] - y_preds_test[i]));
-    }
-    temp/=(dimention);
-    accu_test = temp;
 
-    printf("training accuracy: %d \n", accu_train);
-    printf("testing accuracy: %d \n", accu_test);
+    // printf("training accuracy: %f \n", accu_train);
 
 }
 
 
 
 
+
+
+
 int main() {
-    float w[2];
-    for(int i=0;i<2;i++){
-        scanf("%f",&w[i]);
-    }
-    float b = 1.5;
-    float x[2][3];
-    for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++){
-            scanf("%f",&x[i][j]);
+
+    FILE* file = fopen("mydat.txt","r");
+    float num;
+    for(int j=0;j<50;j++){
+        for(int i=0;i<12288;i++){
+            fscanf(file,"%f",&num);
+
+            x_train[i][j] = (num/255.00);
+            fseek(file,1,SEEK_CUR);
         }
     }
-    float y[3];
-    for(int i=0;i<3;i++){
-        scanf("%f",&y[i]);
+
+
+    FILE* filey = fopen("ydata.txt","r");
+    for(int i=0;i<50;i++){
+        fscanf(filey,"%f",&num);
+        y_train[i] = num;
+        fseek(file,1,SEEK_CUR);
     }
-    // float* dwinfo = propagate(w,b,x,y,2,3);
+
+
+    printaccuracy(12288);
+
+    // initialize weights and biases
+    // float* w = weightinit(12288);
+    // float b = 0.0;
+    // float* dwinfo = propagate(w,b,12288);
+    // float* dwinfo = optimize(w,b,5,0.05,12288);
+
+    // for(int i=0;i<12290;i++){
+    //     printf("%f \n",dwinfo[i]);
+    // }
     // float* infovec = optimize(w,b,x,y,200,0.9,2,3);
-    float* y_preds = predict(w,b,x,2,3);
-    for(int i=0;i<3;i++){
-        printf("%f \n",y_preds[i]);
-    }
+    // float* y_preds = predict(w,b,x,2,3);
+    
     return 0;
 }
